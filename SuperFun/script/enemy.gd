0efadf461caae813ad553ead8920a281ed9b779d
Mainm0e <mainm0e1996@gmail.com>
2024-06-16 20:52:13 +0300

    update

--- SuperFun/script/enemy.gd
+++ SuperFun/script/enemy.gd
@@ -1,62 +1,139 @@
 extends CharacterBody2D
 
-
 const SPEED = 60.0
-const JUMP_VELOCITY = -100.0
-#HitPoint
+const ATTACK_DISTANCE = 20.0
+const ATTACK_COOLDOWN = 0.7
+# HitPoint
 var hitpoint = 100
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var direction = 1
-# Iframe for delay income dmg from player
+# Iframe for delay incoming dmg from player
 var iframe = false
 var attack_dmg = 10
 var is_attacking = false
+var player = null
+var player_detected = false
+
 @onready var ray_cast_left = $RayCastLeft
 @onready var ray_cast_right = $RayCastRight
-@onready var ray_cast_down = $RayCastDown
 @onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var iframe_time = $Timer/iframe
+@onready var attack_cooldown = $Timer/attack_cooldown
+@onready var aoe_detect_time = $"Timer/DetectTime"
+@onready var collision_shape_2d = $CollisionShape2D
+@onready var player_detection_area = $PlayerDetectionArea
+@onready var attack_aoe = $AttackAoe/CollisionShape2D
 
 # Physics system
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
-	# Change sprite direction base on direction input 
+	# Change sprite direction based on direction input
 	if direction > 0:
-		animated_sprite_2d.flip_h = false
+		flip_area_2d(true)
 	elif direction < 0:
-		animated_sprite_2d.flip_h = true
-	
-	if not is_attacking:
+		flip_area_2d(false)
+
+	if not is_attacking and hitpoint > 0:
 		if is_on_floor():
 			if direction == 0:
 				animated_sprite_2d.play("default")
 			else:
 				animated_sprite_2d.play("walk")
-
+	
 	move_and_slide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if hitpoint <= 0:
+		return
+	if player_detected and not is_attacking:
+		chase_player(delta)
+	else:
+		if not is_attacking:
+			patrol(delta)
+
+func patrol(delta):
 	if ray_cast_right.is_colliding():
 		direction = -1
 	if ray_cast_left.is_colliding():
 		direction = 1
-		
-	position.x += direction * SPEED * delta 
+	if hitpoint > 0:
+		position.x += direction * SPEED * delta
+
+func chase_player(delta):
+	if player:
+		var direction_to_player = (player.position - position).normalized()
+		position += direction_to_player * SPEED * delta
+		# Update direction based on player's position
+		if direction_to_player.x > 0:
+			direction = 1
+			flip_area_2d(true)
+		elif direction_to_player.x < 0:
+			direction = -1
+			flip_area_2d(false)
+
+		# Check if within attack distance and initiate attack
+		if position.distance_to(player.position) <= ATTACK_DISTANCE and not is_attacking:
+			attack_player()
+
+func attack_player():
+	is_attacking = true
+	animated_sprite_2d.play("attack")
+	aoe_detect_time.start(0.4)
+	attack_cooldown.start(ATTACK_COOLDOWN)
+
+func _on_attack_cooldown_timeout():
+	is_attacking = false
+	attack_aoe.disabled = true  # Disable the attack AOE after the attack is finished
+
+func _on_attack_aoe_body_entered(body):
+	if hitpoint > 0 and is_attacking and body.is_in_group("Player"):
+		body.get_hit(attack_dmg)  # Assume the player has a get_hit function
+
+func flip_area_2d(is_flipped):
+	# Adjust the position of Area2D or its CollisionShape2D based on the flip state
+	if is_flipped:
+		animated_sprite_2d.flip_h = false
+		attack_aoe.position.x = abs(attack_aoe.position.x)
+	else:
+		animated_sprite_2d.flip_h = true
+		attack_aoe.position.x = -abs(attack_aoe.position.x)
 
 # Take dmg and sub hitpoint and update hitpoint
 func get_hit(dmg):
 	if not iframe:
-		hitpoint = hitpoint - dmg
+		hitpoint -= dmg
 		is_attacking = true
 		animated_sprite_2d.play("hurt")
-		iframe = true
-		iframe_time.start(0.5)
-
+		print("Ohh Hit Point", hitpoint)
+		if hitpoint <= 0:
+			animated_sprite_2d.play("death")
+			# c layer 32 for dead entity
+			collision_layer = 32
+			iframe = true
+		else:
+			iframe = true
+			iframe_time.start(0.5)
 
 func _on_iframe_timeout():
-	iframe = false
-	is_attacking = false
+	if hitpoint > 0:
+		iframe = false
+		is_attacking = false
+
+func _on_player_detection_area_body_entered(body):
+	if body.is_in_group("Player"):
+		player = body
+		player_detected = true
+
+func _on_player_detection_area_body_exited(body):
+	if body == player:
+		player = null
+		player_detected = false
+
+
+func _on_detect_time_timeout():
+	if is_attacking:
+		attack_aoe.disabled = false  # Enable the attack AOE
--- SuperFun/script/player.gd
+++ SuperFun/script/player.gd
@@ -10,13 +10,24 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var hitpoint = 100
 var attack_dmg = 10
 var is_attacking = false
+var got_hit = false
+var i_frame = false
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
-@onready var attack_aoe = $AnimatedSprite2D/AttackAOE/CollisionShape2D
+@onready var attack_aoe = $AttackAOE/CollisionShape2D
 @onready var attack_timer = $timer/AttackTimer
+@onready var hurt = $timer/Hurt
+@onready var iframe = $timer/Iframe
+@onready var restart = $timer/restart
 
+func _process(delta):
+	if hitpoint <= 0:
+		animated_sprite_2d.play("hurt")
+		get_tree().reload_current_scene()
 
 func _physics_process(delta):
+	if hitpoint <= 0:
+		return
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -27,8 +38,8 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	var direction = Input.get_axis("move_L", "move_R")
-	
-	# Change sprite direction base on direction input 
+
+	# Change sprite direction based on direction input
 	if direction > 0:
 		animated_sprite_2d.flip_h = false
 		flip_area_2d(false)
@@ -40,54 +51,78 @@ func _physics_process(delta):
 	if is_attacking and not animated_sprite_2d.is_playing():
 		attack_aoe.disabled = true
 		is_attacking = false
-	
-	# Apply movement and play animations if not attacking
-	if not is_attacking:
-		if Input.is_action_just_pressed("attack_low"):
-			animated_sprite_2d.play("low_attack")
-			attack_dmg = 10
-			attack_timer.start(0.2)
-			is_attacking = true
-			return
 
-		if Input.is_action_just_pressed("attack_heavy"):
-			animated_sprite_2d.play("heavy_attack")
-			attack_dmg = 25
-			attack_timer.start(0.2)
-			is_attacking = true
-			return
+	# Handle player attacks
+	if not is_attacking and not got_hit:
+		handle_attacks()
 
-		if is_on_floor():
-			if direction == 0:
-				animated_sprite_2d.play("idle")
-			else:
-				animated_sprite_2d.play("walk")
-		
+	# Apply movement and play animations if not attacking or hit
+	if not is_attacking and not got_hit:
+		handle_movement_animations(direction)
+
 	# Apply movement 
-	if direction:
+	if direction != 0 and not got_hit:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
 
+# Handle player attacks
+func handle_attacks():
+	if Input.is_action_just_pressed("attack_low"):
+		animated_sprite_2d.play("low_attack")
+		attack_dmg = 10
+		attack_timer.start(0.2)
+		is_attacking = true
+	elif Input.is_action_just_pressed("attack_heavy"):
+		animated_sprite_2d.play("heavy_attack")
+		attack_dmg = 25
+		attack_timer.start(0.2)
+		is_attacking = true
+
+# Handle movement animations
+func handle_movement_animations(direction):
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite_2d.play("idle")
+		else:
+			animated_sprite_2d.play("walk")
 
 func flip_area_2d(is_flipped):
-	# Adjust the position of Area2D or its CollisionShape2D based on the flip state
+	 #Adjust the position of Area2D or its CollisionShape2D based on the flip state
 	if is_flipped:
 		attack_aoe.position.x = -abs(attack_aoe.position.x)
 	else:
 		attack_aoe.position.x = abs(attack_aoe.position.x)
-
-
+		
+func get_hit(dmg):
+	if hitpoint <= 0:
+		return
+	if not i_frame:
+		got_hit = true
+		i_frame = true
+		hitpoint = hitpoint - dmg
+		animated_sprite_2d.play("hurt")
+		hurt.start(0.5)
+		iframe.start(1.0)
 # animation delay
 func _on_attack_timer_timeout():
 	attack_aoe.disabled = false
 	
 
 func _on_attack_aoe_body_entered(body):
-		if body.is_in_group("Hit"):
+		if body.is_in_group("Enemy"):
 			body.get_hit(attack_dmg)
-		
+	
+
+func _on_hurt_timeout():
+	got_hit = false
 
 
+func _on_iframe_timeout():
+	i_frame = false 
+
+
+func _on_restart_timeout():
+	print("dead")
